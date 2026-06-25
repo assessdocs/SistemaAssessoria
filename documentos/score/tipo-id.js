f0da02df6dfad1aacea2d0eef07ec60330dbb921
assessdocs <198103248+assessdocs@users.noreply.github.com>
2026-06-25 17:27:36 -0300

    Update tipo-id.js

--- documentos/score/tipo-id.js
+++ documentos/score/tipo-id.js
@@ -5,18 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreTitulo = document.getElementById('score-titulo');
     const versaoManual = document.getElementById('i-versaomanual');
 
+    function cpf() {
+        identificacao.textContent = 'CPF:';
+        scoreTitulo.textContent = 'Score do CPF consultado';
+
+        versaoManual.selected = true;
+        versaoManual.dispatchEvent(new Event('change', { bubbles: true }));
+    }
+
+    function cnpj() {
+        identificacao.textContent = 'CNPJ:';
+        scoreTitulo.textContent = 'Score do CNPJ consultado';
+    }
+
     function atualizarTipoId() {
         const selecionado = document.querySelector('input[name="tipo-id"]:checked')?.value;
 
         if (selecionado === 'cpf') {
-            identificacao.textContent = 'CPF:';
-            scoreTitulo.textContent = 'Score do CPF consultado';
-            versaoManual.selected = true;
-            versaoManual.checked = true;
-            versaoManual.dispatchEvent(new Event('change', { bubbles: true }));
+            cpf();
         } else {
-            identificacao.textContent = 'CNPJ:';
-            scoreTitulo.textContent = 'Score do CNPJ consultado';
+            cnpj();
         }
     }
 
